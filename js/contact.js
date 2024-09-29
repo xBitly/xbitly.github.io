c583e6683e5fe1a46127f5b1728eace896261ba6
xBitly <xBitly@mail.ru>
2024-09-29 13:21:47 +0300

    Fix form

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,4 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
+document.getElementById('contact-form').addEventListener('submit', async function (event) {
     event.preventDefault();
 
     const formData = new FormData(this);
@@ -9,21 +9,24 @@ document.getElementById('contact-form').addEventListener('submit', function(even
         data[key] = value;
     });
 
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbxZNkzd2PIRYdVTDB3HOgMsa-9VKmgqilZiXnNMsgd55NocLilkuoZU9f0zYUdGWeK3/exec';
-
-    fetch(scriptURL, {
-        method: 'POST',
-        body: new URLSearchParams(data)
-    })
-        .then(response => response.json())
-        .then(data => {
-            if (data.result === 'success') {
-                alert('Заявка успешно отправлена!');
-            } else {
-                alert('Ошибка при отправке заявки');
-            }
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbzFJSrT9hkIfFm960_-zDPmxfkzen2OytSncCo_1M6z7qiZM-uZU32489H4X3mIqN24/exec'
+    try {
+        const response = await fetch(scriptURL, {
+            redirect: "follow",
+            method: 'POST',
+            mode: "no-cors",
+            body: new URLSearchParams(data),
         })
-        .catch(error => {
-            alert('Произошла ошибка: ' + error);
-        });
+    } catch (error) {
+        console.log(error)
+    } finally {
+        const messageBox = document.getElementById('success-message');
+        messageBox.classList.remove('hidden');
+        messageBox.classList.add('show');
+
+        setTimeout(() => {
+            messageBox.classList.remove('show');
+            messageBox.classList.add('hidden');
+        }, 3000);
+    }
 });
